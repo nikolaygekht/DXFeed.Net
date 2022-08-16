f901ef8081f8ca044800e8703c7790e88e5b6a24
Nikolay Gekht <nikolay.gekht@gehtsoftusa.com>
2022-08-16 16:38:51 -0400

    update docs

---
+++ DXFeed.Net/DXFeedMessage/DXFeedSubscribeMode.cs
@@ -0,0 +1,17 @@
+﻿namespace DXFeed.Net.DXFeedMessage
+{
+    /// <summary>
+    /// Subscription mode
+    /// </summary>
+    public enum DXFeedSubscribeMode
+    {
+        /// <summary>
+        /// Add subscription
+        /// </summary>
+        Add,
+        /// <summary>
+        /// Remove subscription
+        /// </summary>
+        Remove,
+    }
+}
